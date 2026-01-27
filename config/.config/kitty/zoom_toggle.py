ef15a8e46d7665784da5db1adda0d76e1a959b65
Oswin So <oswinso@gmail.com>
2026-01-27 17:57:09 -0500

    asdf

---
+++ config/.config/kitty/zoom_toggle.py
@@ -0,0 +1,15 @@
+from kitty.boss import Boss
+
+def main(args: list[str]) -> str:
+    return ""
+
+def handle_result(
+    args: list[str], answer: str, target_window_id: int, boss: Boss
+) -> None:
+    tab = boss.active_tab
+    if tab is not None:
+        if tab.current_layout.name == "stack":
+            tab.last_used_layout()
+        else:
+            tab.goto_layout("stack")
+
